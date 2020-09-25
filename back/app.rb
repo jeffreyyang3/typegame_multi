6c8a6c817640fce816ba503ebc5550fba2739fe2
Jeffrey Yang <jeffreyhasmail@gmail.com>
2020-09-25 16:44:30 -0700

    ws testing

--- back/app.rb
+++ back/app.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/json'
+require 'json'
 require 'sinatra-websocket'
 
 set :server, 'thin'
 set :sockets, []
 set :bind, '0.0.0.0'
+state = { :messages => [] }
 
 get '/hello' do
-  "hello dfdfd"
+  "hello changed2"
 end
 
 
@@ -17,11 +19,20 @@ get '/x' do
   json ob
 end
 
+
 get '/ws' do
   request.websocket do |ws|
     ws.onopen do
-      ws.send('yo')
+      ws.send('yo4')
       settings.sockets << ws
+    end
+    # ws.onmessage do |msg|
+    #   puts msg
+    # end
+    ws.onmessage do |msgJson|
+      parse = JSON.parse msgJson
+      state[:messages].push parse['el']
+      puts state
     end
   end
 end
